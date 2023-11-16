010e9f91a4cb2235c992d65641d388b3692aab0f
Krytog <danya2003.db@gmail.com>
2023-11-16 18:38:47 +0300

    Add basic main.cpp for tests

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,25 @@
+#define GLFW_INCLUDE_NONE
 #include <iostream>
+#include <GLFW/glfw3.h>
+#include <GUI/Window/window.h>
+#include <Utils/LightTimer/LightTimer.h>
+#include <thread>
+
+
 
 int main(int argc, char** argv) {
-	std::cout << "Hello world!" << std::endl;
+	Window window(640, 480, "Test window!");
+
+	const int FPS = 144;
+	const double frametime = 1.0f / FPS;
+	LightTimer time;
+
+	while (!window.ShouldWindowBeClosed()) {
+		time.ResetTime();
+
+		glfwPollEvents();
+
+		std::this_thread::sleep_for(std::chrono::microseconds(int(1000000 * (frametime - time.EvaluateTime()))));
+	}
 	return 0;
 }
