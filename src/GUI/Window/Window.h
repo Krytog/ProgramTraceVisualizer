fac28a91aff6038805d6d9b893bdf5af408a471c
Krytog <danya2003.db@gmail.com>
2023-12-10 16:12:03 +0300

    Refactor comments in Window.h

--- src/GUI/Window/Window.h
+++ src/GUI/Window/Window.h
@@ -7,23 +7,22 @@ class Window {
 public:
 	Window(int width, int height, const char* title);
 
-	// Returns whether the window is pending to be closed (the user has pressed alt + f4 or cross)
+	/* Returns whether the window is pending to be closed(the user has pressed alt + f4 or cross) */
 	bool IsPendingClose() const;
 
-	// Makes the context of the window to be the current Openl GL contex. If should_init_opengl = true,
-	// then it also inits (finds the function pointers) OpenGL (via GLEW). The initialization should be done at least once. 
+	/* Makes the context of the window to be the current Openl GL contex.If should_init_opengl = true,
+	then it also inits (finds the function pointers) OpenGL (via GLEW). The initialization should be done at least once. */
 	void CaptureContext(bool should_init_opengl = false) const;
 
-	// Returns the current size of the window
+	/* Returns the current size of the window */
 	[[nodiscard]] std::pair<int, int> GetWindowSize() const;
 
-	// Swaps rendering buffers, thereby showing updated buffer on the screen
+	/* Swaps rendering buffers, thereby showing updated buffer on the screen */
 	void Render() const;
 
 	~Window();
 private:
-
-	// Set hints for GLFW
+	/* Set hints for GLFW */
 	static void SetHints();
 
 	static size_t kCounter_;
