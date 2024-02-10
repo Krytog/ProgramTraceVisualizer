52697666db500400729c99d4d971b66f62a99b0b
Krytog <danya2003.db@gmail.com>
2024-02-10 04:30:10 +0300

    Update main.cpp for testing

--- src/main.cpp
+++ src/main.cpp
@@ -3,21 +3,13 @@
 #include <iostream>
 #include <GLFW/glfw3.h>
 #include <GUI/Window/Window.h>
-#include <Graphics/Shader/Shader.h>
 #include <Utils/LightTimer/LightTimer.h>
 #include <thread>
-#include <Graphics/VertexObjects/VertexObjectIndexed.h>
 #include <UI/UIManager/UIManager.h>
-#include <UI/ViewScene/ViewScene.h>
-#include <Graphics/RenderBuffer/RenderBuffer.h>
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
-#include <filesystem>
 #include <Graphics/Primitives/Cube/Cube.h>
-
-const GLchar* vertexShaderSource = "../src/Resources/Shaders/default.vert";
-const GLchar* fragmentShaderSource = "../src/Resources/Shaders/default.frag";
 
 int main(int argc, char** argv) {
 	Window window(1000, 800, "Test window!");
@@ -27,48 +19,27 @@ int main(int argc, char** argv) {
 	const double frametime = 1.0f / FPS;
 	LightTimer time;
 
-    Shader::ShaderConfig config;
-    config.VertexShaderSourceType = Shader::SourceType::FILE;
-    config.VertexShaderSource = vertexShaderSource;
-    config.FragmentShaderSourceType = Shader::SourceType::FILE;
-    config.FragmentShaderSource = fragmentShaderSource;
-    config.GeometryShaderSourceType = Shader::SourceType::NONE;
-    config.GeometryShaderSource = "aboba/none.nothing";
-
-    Shader shader(config);
-
-    GLfloat vertices[] = { -0.3, -0.3, 0.0, 1.0, 1.0,
-                            0.3, -0.3, 0.0, 1.0, 0.0, 
-                            0.3, 0.3, 0.0, 0.0, 1.0, 
-                            -0.3, 0.3, 0.0, 0.0, 0.0 };
-
-    GLuint indices[] = { 0, 1, 2, 0, 2, 3 };
-
-    VertexObjectIndexed::ArgPack arg_pack;
-    arg_pack.data = vertices;
-    arg_pack.data_size = sizeof(vertices);
-    arg_pack.args_per_vertex = 5;
-    arg_pack.indices = indices;
-    arg_pack.indices_size = sizeof(indices);
-    arg_pack.memory_mode = VertexObject::MemoryMode::STATIC;
-
-    VertexObjectIndexed figure(arg_pack);
-    figure.SetAttribute(0, 3, static_cast<GLvoid*>(0));
-    figure.SetAttribute(1, 2, reinterpret_cast<GLvoid*>(3 * sizeof(GLfloat)));
-
     auto ptr = window.GetInnerWindowPointer();
     UIManager ui_manager(ptr);
-    ViewScene scene;
 
     glm::mat4 matrix = glm::mat4(1.0f);
 
-    Cube cube;
+    std::shared_ptr<Cube> cube = std::make_shared<Cube>();
 
-    RenderBuffer render_buffer(1000, 800);
+    ui_manager.GetViewScene().AddObject(cube);
 
     glEnable(GL_DEPTH_TEST);
 
     LightTimer run_timer;
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(1500));
+
+    std::shared_ptr<Cube> cube2 = std::make_shared<Cube>();
+
+    ui_manager.GetViewScene().AddObject(cube2);
+
+    LightTimer run_timer2;
+
 	while (!window.IsPendingClose()) {
 		time.ResetTime();
 
@@ -78,18 +49,25 @@ int main(int argc, char** argv) {
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        auto transform_matrix = glm::rotate(matrix, static_cast<float>(run_timer.EvaluateTime()), glm::vec3(0.3f, 1.0f, 0.0f));
-        transform_matrix = glm::translate(transform_matrix, 2.5f * glm::vec3(std::sin(run_timer.EvaluateTime()), std::cos(run_timer.EvaluateTime()), 0.0f));
-        auto view_matrix = glm::translate(matrix, glm::vec3(0.0f, 0.0f, -5.0f));
-        glm::mat4 projection_matrix = glm::perspective(45.0f, static_cast<float>(size.first) / size.second, 0.1f, 100.0f);
-        auto final_matrix = projection_matrix * view_matrix * transform_matrix;
-        cube.SetTransform(final_matrix);
-        render_buffer.Bind();
-        render_buffer.Clear();
-        cube.Render();
-        render_buffer.Unbind();
+        {
+            auto transform_matrix = glm::rotate(matrix, static_cast<float>(run_timer.EvaluateTime()), glm::vec3(0.3f, 1.0f, 0.0f));
+            transform_matrix = glm::translate(transform_matrix, 2.5f * glm::vec3(std::sin(run_timer.EvaluateTime()), std::cos(run_timer.EvaluateTime()), 0.0f));
+            auto view_matrix = glm::translate(matrix, glm::vec3(0.0f, 0.0f, -5.0f));
+            glm::mat4 projection_matrix = glm::perspective(45.0f, static_cast<float>(size.first) / size.second, 0.1f, 100.0f);
+            auto final_matrix = projection_matrix * view_matrix * transform_matrix;
+            cube->SetTransform(final_matrix);
+        }
+        {
+            auto transform_matrix = glm::rotate(matrix, static_cast<float>(run_timer2.EvaluateTime()), glm::vec3(0.3f, 1.0f, 0.0f));
+            transform_matrix = glm::translate(transform_matrix, 2.5f * glm::vec3(std::sin(run_timer2.EvaluateTime()), std::cos(run_timer2.EvaluateTime()), 0.0f));
+            auto view_matrix = glm::translate(matrix, glm::vec3(0.0f, 0.0f, -5.0f));
+            glm::mat4 projection_matrix = glm::perspective(45.0f, static_cast<float>(size.first) / size.second, 0.1f, 100.0f);
+            auto final_matrix = projection_matrix * view_matrix * transform_matrix;
+            cube2->SetTransform(final_matrix);
+        }
+        
 
-        ui_manager.DrawUI(render_buffer.GetTextureID());
+        ui_manager.DrawUI();
 
 		window.Render();
 		glfwPollEvents();
