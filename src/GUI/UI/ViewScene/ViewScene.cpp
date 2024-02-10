0634585cbd4cad31c2221998ec241c1e853b2f61
Krytog <danya2003.db@gmail.com>
2024-02-10 04:29:25 +0300

    Make ViewScene work

--- src/GUI/UI/ViewScene/ViewScene.cpp
+++ src/GUI/UI/ViewScene/ViewScene.cpp
@@ -1,12 +1,12 @@
 #include "ViewScene.h"
 
-#include <Primitives/IRenderable.h>
+#include <Graphics/Primitives/IRenderable.h>
 #include <imgui.h>
 #include <backends/imgui_impl_glfw.h>
 #include <backends/imgui_impl_opengl3.h>
 #include <GLFW/glfw3.h>
 
-ViewScene::ViewScene() = default;
+ViewScene::ViewScene(GLint width, GLint height): render_buffer_(width, height) {}
 
 bool ViewScene::AddObject(const std::shared_ptr<IRenderable>& object) {
 	bool output = true;
@@ -26,18 +26,34 @@ bool ViewScene::RemoveObject(const std::shared_ptr<IRenderable>& object) {
 	return true;
 }
 
-void ViewScene::Render() const {
-	MarkUpUI();
-	RenderAll();
+void ViewScene::Render() {
+	RenderObjects();
+	RenderUI();
 }
 
-void ViewScene::RenderAll() const {
+void ViewScene::RenderObjects() const {
+	render_buffer_.Bind();
+	render_buffer_.Clear();
 	for (const auto& renderable : renderable_objects_) {
 		renderable->Render();
 	}
+	render_buffer_.Unbind();
 }
 
-void ViewScene::MarkUpUI() const {
+void ViewScene::RenderUI() {
 	ImGui::Begin("View");
+	auto current_size = TryResize();
+	const GLuint texture_id = render_buffer_.GetTextureID();
+	const ImVec2 frame_size(static_cast<float>(current_size.first), static_cast<float>(current_size.second));
+	ImGui::Image(reinterpret_cast<void*>(texture_id), frame_size, ImVec2{ 0, 1 }, ImVec2{ 1, 0 });
 	ImGui::End();
+}
+
+std::pair<GLint, GLint> ViewScene::TryResize() {
+	auto outer_size = ImGui::GetContentRegionAvail();
+	auto inner_size = render_buffer_.GetSize();
+	if (outer_size.x != inner_size.first || outer_size.y != inner_size.second) {
+		render_buffer_.Resize(outer_size.x, outer_size.y);
+	}
+	return { outer_size.x, outer_size.y };
 }
--- src/GUI/UI/ViewScene/ViewScene.h
+++ src/GUI/UI/ViewScene/ViewScene.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <Graphics/RenderBuffer/RenderBuffer.h>
+
 #include <unordered_set>
 #include <memory>
 
@@ -7,7 +9,7 @@ class IRenderable;	// Forward declaration
 
 class ViewScene {
 public:
-	ViewScene();
+	ViewScene(GLint width, GLint height);
 
 	/* Callback triggered when mouse moves */
 	void OnMouseMove(double x, double y);
@@ -16,7 +18,7 @@ public:
 	void OnMouseScroll(double delta);
 
 	/* Renders the scene*/
-	void Render() const;
+	void Render();
 
 	/* Adds an object to this scene. Returns true if the object is added for the first time and false otherwise */
 	bool AddObject(const std::shared_ptr<IRenderable>& object);
@@ -25,11 +27,15 @@ public:
 	bool RemoveObject(const std::shared_ptr<IRenderable>& object);
 
 private:
-	/* Renders all object */
-	void RenderAll() const;
+	/* Renders (only into the inner buffer) all renderable objects, added to this scene */
+	void RenderObjects() const;
 
-	/* Renders UI elements of the scene */
-	void MarkUpUI() const;
+	/* Renders UI elements of the scene, draws the content of the inner buffer */
+	void RenderUI();
+
+	/* Resizes the underlying rendering buffer if the widget's size doesn't fit. Returns the current size */
+	std::pair<GLint, GLint> TryResize();
 
 	std::unordered_set<std::shared_ptr<IRenderable>> renderable_objects_;
+	RenderBuffer render_buffer_;
 };
