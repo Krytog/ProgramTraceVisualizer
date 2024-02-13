17e1dc44b887b3f428c021e0b791039cd445b58c
Krytog <danya2003.db@gmail.com>
2024-02-13 07:01:50 +0300

    Add Plot2DMesh

---
+++ src/Core/Plotting/Plot2DMesh/Plot2DMesh.cpp
@@ -0,0 +1,41 @@
+#include "Plot2DMesh.h"
+
+namespace {
+	static constexpr const VertexObject::ArgPack kVertexObjectConfig{.data = nullptr, .data_size = 0, 
+		.args_per_vertex = 3,.memory_mode = VertexObject::MemoryMode::STREAM };
+
+	static constexpr const GLchar* kVertexShaderPath = "../src/Resources/Shaders/Plot2D.vert";
+	static constexpr const GLchar* kFragmentShaderPath = "../src/Resources/Shaders/Plot2D.frag";
+	static constexpr const GLchar* kGeometryShaderPath = "../src/Resources/Shaders/Plot2D.geom";
+
+	static constexpr const Shader::ShaderConfig kShaderConfig{.VertexShaderSourceType = Shader::SourceType::FILE, .VertexShaderSource = kVertexShaderPath,
+		.FragmentShaderSourceType = Shader::SourceType::FILE, .FragmentShaderSource = kFragmentShaderPath,
+		.GeometryShaderSourceType = Shader::SourceType::FILE, .GeometryShaderSource = kGeometryShaderPath};
+
+	static constexpr const GLuint kPositionIndex = 0;
+	static constexpr const GLuint kTemperatureIndex = 1;
+	static constexpr const GLuint kPositionArgs = 2;
+	static constexpr const GLuint kTemperatureArgs = 1;
+}
+
+Plot2DMesh::Plot2DMesh(const size_t grid_size) : grid_size_(grid_size), shader_(kShaderConfig), mesh_(kVertexObjectConfig) {
+	mesh_.SetAttribute(kPositionIndex, kPositionArgs, reinterpret_cast<GLvoid*>(0)); // position
+	mesh_.SetAttribute(kTemperatureIndex, kTemperatureArgs, reinterpret_cast<GLvoid*>(kPositionArgs * sizeof(GLfloat))); // color
+}
+
+void Plot2DMesh::Render() const {
+	shader_.Use();
+	mesh_.Draw(VertexObject::DrawMode::POINTS);
+}
+
+void Plot2DMesh::SetGridSize(const size_t grid_size) {
+	grid_size_ = grid_size;
+}
+
+size_t Plot2DMesh::GetGridSize() const {
+	return grid_size_;
+}
+
+void Plot2DMesh::LoadData(const GLfloat* data, GLsizei data_size) {
+	mesh_.LoadData(data, data_size, VertexObject::MemoryMode::STREAM);
+}
---
+++ src/Core/Plotting/Plot2DMesh/Plot2DMesh.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <Graphics/VertexObjects/VertexObject.h>
+#include <Graphics/Shader/Shader.h>
+#include <Graphics/Primitives/IRenderable.h>
+
+#include <cstddef>
+
+class Plot2DMesh : public IRenderable {
+public:
+	Plot2DMesh(const size_t grid_size);
+
+	/* Renders the plot mesh */
+	void Render() const override;
+
+	/* Sets new grid_size */
+	void SetGridSize(const size_t grid_size);
+
+	/* Returns the current grid_size */
+	size_t GetGridSize() const;
+
+	/* Loads new data to be rendered. Data format is as follows: [x, y, temperature] per vertex, for a total of grid_size * grid_size vertices */
+	void LoadData(const GLfloat* data, GLsizei data_size);
+
+private:
+	size_t grid_size_;
+
+	Shader shader_;
+	VertexObject mesh_;
+};
