cb54dfdc0fa18a5ef64ece5ba0e116eab982d3d7
Krytog <danya2003.db@gmail.com>
2024-03-18 19:54:44 +0300

    Add SlidingWindow for IPmoves

---
+++ src/Core/IPmoves/SlidingWindow/SlidingWindowIterator.h
@@ -0,0 +1,49 @@
+#pragma once
+
+#include <vector>
+#include <utility>
+
+template <typename Container>
+class SlidingWindowIterator {
+	using Iterator = Container::iterator;
+
+public:
+	SlidingWindowIterator(Container& data, size_t window_size): 
+		container_begin_(data.begin()),
+		container_end_(data.end()),
+		begin_(data.begin()),
+		window_size_(window_size)
+	{}
+
+	[[nodiscard]] std::pair<Iterator, Iterator> operator*() const {
+		return { begin_, begin_ + window_size_ };
+	}
+
+	[[nodiscard]] operator bool() const {
+		return begin_ + window_size_ <= container_end_;
+	}
+
+	SlidingWindowIterator& operator+=(size_t offset) {
+		begin_ += offset;
+		return *this;
+	}
+
+	SlidingWindowIterator& operator-=(size_t offset) {
+		begin_ -= offset;
+		if (begin_ < container_begin_) {
+			begin_ = container_begin_;
+		}
+		return *this;
+	}
+
+	SlidingWindowIterator& operator++() {
+		++begin_;
+		return* this;
+	}
+
+private:
+	const Iterator container_begin_;
+	const Iterator container_end_;
+	Iterator begin_;
+	size_t window_size_;
+};
