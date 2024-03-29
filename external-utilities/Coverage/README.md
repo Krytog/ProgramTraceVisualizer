07afaec1b2398eb8d85ecb8d15ab3ff43cef41dc
Daniil Baranov <danya2003.db@gmail.com>
2024-03-30 00:21:39 +0300

    Fix external coverage

--- external-utilities/Coverage/README.md
+++ external-utilities/Coverage/README.md
@@ -4,6 +4,7 @@ If there's access to the source code of the target program, its trace can be cap
 
 ## Usage 
 * The target program should be compiled with clang. Usually, it's enough to add ```set(CMAKE_CXX_COMPILER clang++)``` and ```set(CMAKE_C_COMPILER clang)``` before ```project()``` call in the CMakeLists.txt of the target.
+* Add ```set(CMAKE_CXX_FLAGS -fsanitize-coverage=func,trace-pc-guard)``` (or equivalent). Notice that it's possible to choose targets to which coverage is added
 * Add this directory as a subdirectory in the CMakeLists.txt of the target: ```add_subdirectory(<path_to_this_folder>)```.
 * Build the target.
 * On success, when the target runs, a ```captured_ip.trace``` file is created, into which the trace is printed.
--- external-utilities/Coverage/covers.cpp
+++ external-utilities/Coverage/covers.cpp
@@ -1,17 +1,20 @@
+#include <cstdio>
 #include "FileLogger.h"
 
 #define COVERAGE_LOG_FILENAME "captured_ip.trace"
-#define BUFFER_SIZE 1048576ull
+#define BUFFER_SIZE 134217728ull
 
 static thread_local bool log_guard = false;
 static bool log_alive = false;
 
 static FileLogger* logger;
 
+
 extern "C" void __sanitizer_cov_trace_pc_guard_init(uint32_t* start, uint32_t* stop) {  // NOLINT
     if (start == stop || *start || log_alive) {
         return;
     }
+    printf("we're cool %ld\n", stop - start);
     static FileLogger logger_object(COVERAGE_LOG_FILENAME, BUFFER_SIZE, &log_alive);
     logger = &logger_object;
     log_alive = true;
@@ -23,8 +26,7 @@ extern "C" void __sanitizer_cov_trace_pc_guard(uint32_t* guard) {  // NOLINT
     }
     (void)guard;  // in this implementation guards are omitted
     void* pc = __builtin_return_address(0);
-    log_guard =
-        true;  // this is done to prevent recursively falling into this function from within logger->Write
+    log_guard = true;  // this is done to prevent recursive calls of this function from within logger->Write
     logger->Write((const char*)&pc, sizeof(void*));
     log_guard = false;
 }
