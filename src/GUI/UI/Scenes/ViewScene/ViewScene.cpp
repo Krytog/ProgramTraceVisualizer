cdd7ec067174891b3dd7c4db35e76a74ab8b8bd5
Daniil Baranov <danya2003.db@gmail.com>
2024-03-30 14:07:18 +0300

    clang-format ViewScene

--- src/GUI/UI/Scenes/ViewScene/ViewScene.cpp
+++ src/GUI/UI/Scenes/ViewScene/ViewScene.cpp
@@ -7,58 +7,60 @@
 
 #define ViewScene_InnerName "View"
 
-ViewScene::ViewScene(const std::pair<float, float>& position, const std::pair<float, float>& size): BasicScene(position, size, ViewScene_InnerName),
-																									render_buffer_(size.first, size.second) {}
+ViewScene::ViewScene(const std::pair<float, float>& position, const std::pair<float, float>& size)
+    : BasicScene(position, size, ViewScene_InnerName), render_buffer_(size.first, size.second) {
+}
 
 bool ViewScene::AddObject(const std::shared_ptr<IRenderable>& object) {
-	bool output = true;
-	if (renderable_objects_.contains(object)) {
-		output = false;
-	}
-	renderable_objects_.insert(object);
-	return output;
+    bool output = true;
+    if (renderable_objects_.contains(object)) {
+        output = false;
+    }
+    renderable_objects_.insert(object);
+    return output;
 }
 
 bool ViewScene::RemoveObject(const std::shared_ptr<IRenderable>& object) {
-	auto iterator = renderable_objects_.find(object);
-	if (iterator == renderable_objects_.end()) {
-		return false;
-	}
-	renderable_objects_.erase(iterator);
-	return true;
+    auto iterator = renderable_objects_.find(object);
+    if (iterator == renderable_objects_.end()) {
+        return false;
+    }
+    renderable_objects_.erase(iterator);
+    return true;
 }
 
 void ViewScene::RenderInner() const {
-	RenderObjects();
-	RenderUI();
+    RenderObjects();
+    RenderUI();
 }
 
 void ViewScene::RenderObjects() const {
-	render_timer_.ResetTime();
-	render_buffer_.Bind();
-	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-	render_buffer_.Clear();
-	for (const auto& renderable : renderable_objects_) {
-		renderable->Render();
-	}
-	render_buffer_.Unbind();
+    render_timer_.ResetTime();
+    render_buffer_.Bind();
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+    render_buffer_.Clear();
+    for (const auto& renderable : renderable_objects_) {
+        renderable->Render();
+    }
+    render_buffer_.Unbind();
 }
 
 void ViewScene::RenderUI() const {
-	const double current_frametime = render_timer_.EvaluateTime();
-	const unsigned long long current_framerate = 1.0 / current_frametime;
-	ImGui::Text(ViewScene_InnerName " \t FrameTime: %.8f \t FPS: %llu", current_frametime, current_framerate);
-	const float text_height_offset = 30.0f; // it's magic constant 
+    const double current_frametime = render_timer_.EvaluateTime();
+    const unsigned long long current_framerate = 1.0 / current_frametime;
+    ImGui::Text(ViewScene_InnerName " \t FrameTime: %.8f \t FPS: %llu", current_frametime, current_framerate);
+    const float text_height_offset = 30.0f;  // it's magic constant
 
-	const GLuint texture_id = render_buffer_.GetTextureID();
-	const long long unsigned texture_id_upcast = static_cast<long long unsigned>(texture_id); // upcasting GLuint to a bigger intenger type for safety reasons
-	ImGui::Image(reinterpret_cast<void*>(texture_id_upcast), ImVec2{ size_.first, size_.second - text_height_offset }, 
-				 ImVec2{ 0, 1 }, ImVec2{ 1, 0 });
+    const GLuint texture_id = render_buffer_.GetTextureID();
+    const long long unsigned texture_id_upcast = static_cast<long long unsigned>(
+        texture_id);  // upcasting GLuint to a bigger intenger type for safety reasons
+    ImGui::Image(reinterpret_cast<void*>(texture_id_upcast),
+                 ImVec2{size_.first, size_.second - text_height_offset}, ImVec2{0, 1}, ImVec2{1, 0});
 }
 
 void ViewScene::Resize(const std::pair<float, float>& new_size) {
-	if (new_size.first != size_.first || new_size.second != size_.second) {
-		render_buffer_.Resize(new_size.first, new_size.second);
-		size_ = new_size;
-	}
+    if (new_size.first != size_.first || new_size.second != size_.second) {
+        render_buffer_.Resize(new_size.first, new_size.second);
+        size_ = new_size;
+    }
 }
--- src/GUI/UI/Scenes/ViewScene/ViewScene.h
+++ src/GUI/UI/Scenes/ViewScene/ViewScene.h
@@ -7,37 +7,39 @@
 #include <unordered_set>
 #include <memory>
 
-class IRenderable;	// Forward declaration
+class IRenderable;  // Forward declaration
 
 class ViewScene : public BasicScene {
 public:
-	ViewScene(const std::pair<float, float>& position, const std::pair<float, float>& size);
+    ViewScene(const std::pair<float, float>& position, const std::pair<float, float>& size);
 
-	/* Callback triggered when mouse moves */
-	void OnMouseMove(double x, double y);
+    /* Callback triggered when mouse moves */
+    void OnMouseMove(double x, double y);
 
-	/* Callback triggered when mouse scrolls */
-	void OnMouseScroll(double delta);
+    /* Callback triggered when mouse scrolls */
+    void OnMouseScroll(double delta);
 
-	/* Resizes the scene and the underlaying render buffer */
-	void Resize(const std::pair<float, float>& new_size);
+    /* Resizes the scene and the underlaying render buffer */
+    void Resize(const std::pair<float, float>& new_size);
 
-	/* Adds an object to this scene. Returns true if the object is added for the first time and false otherwise */
-	bool AddObject(const std::shared_ptr<IRenderable>& object);
+    /* Adds an object to this scene. Returns true if the object is added for the first time and false
+     * otherwise */
+    bool AddObject(const std::shared_ptr<IRenderable>& object);
 
-	/* Removes an object from this scene. Returns false if the object isn't present and true otherwise */
-	bool RemoveObject(const std::shared_ptr<IRenderable>& object);
+    /* Removes an object from this scene. Returns false if the object isn't present and true otherwise */
+    bool RemoveObject(const std::shared_ptr<IRenderable>& object);
+
 private:
-	/* Renders the scene*/
-	void RenderInner() const override;
+    /* Renders the scene*/
+    void RenderInner() const override;
 
-	/* Renders (only into the inner buffer) all renderable objects, added to this scene */
-	void RenderObjects() const;
+    /* Renders (only into the inner buffer) all renderable objects, added to this scene */
+    void RenderObjects() const;
 
-	/* Renders UI elements of the scene, draws the content of the inner buffer */
-	void RenderUI() const;
+    /* Renders UI elements of the scene, draws the content of the inner buffer */
+    void RenderUI() const;
 
-	std::unordered_set<std::shared_ptr<IRenderable>> renderable_objects_;
-	RenderBuffer render_buffer_;
-	mutable LightTimer render_timer_;
+    std::unordered_set<std::shared_ptr<IRenderable>> renderable_objects_;
+    RenderBuffer render_buffer_;
+    mutable LightTimer render_timer_;
 };
