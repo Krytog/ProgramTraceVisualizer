c2f6d00c9a983fccfd420243a80fe3f7a6d80f3f
Daniil Baranov <danya2003.db@gmail.com>
2024-04-12 04:58:05 +0300

    Add LazyFileBuffer

---
+++ src/Utils/LazyFileBuffer/LazyFileBuffer.cpp
@@ -0,0 +1,17 @@
+#include "LazyFileBuffer.h"
+
+LazyFileBuffer::LazyFileBuffer(const std::string& name): file_(name) {}
+
+void LazyFileBuffer::SetMaxMemory(size_t max_memory) {
+    chunk_size_ = max_memory;
+}
+
+void LazyFileBuffer::LoadChunk(size_t position) {
+    auto [bytes, size] = file_.GetChunk(position, chunk_size_);
+    Chunk chunk{
+        .from = position,
+        .to = position + size,
+        .bytes = std::move(bytes)
+    };
+    chunk_ = std::move(chunk);
+}
---
+++ src/Utils/LazyFileBuffer/LazyFileBuffer.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include <FileReader/FileReader.h>
+
+#include <memory>
+#include <cassert>
+
+class LazyFileBuffer {
+public:
+    LazyFileBuffer(const std::string& filename);
+    LazyFileBuffer(const LazyFileBuffer& other) = delete;
+    LazyFileBuffer& operator=(const LazyFileBuffer& other) = delete;
+
+    /* Returns an object at the given position as T type. May modify the underlying caching buffer */
+    template <typename T>
+    [[nodiscard]] T GetAs(size_t position) {
+        assert(sizeof(T) <= chunk_size_);
+        char* object_bytes = GetAtPos<T>(position);
+        return *reinterpret_cast<T*>(object_bytes);
+    }
+
+    /* Sets the max amount of memory that the underlaying cache can use */
+    void SetMaxMemory(size_t max_memory);
+
+private:
+    /* Returns a pointer to location of at least sizeof(T) bytes, starting from the byte at the given position */
+    template <typename T>
+    [[nodiscard]] char* GetAtPos(size_t position) {
+    const size_t end_position = position + sizeof(T);
+    if (position < chunk_.from || end_position > chunk_.to) {
+        LoadChunk(position);
+    }
+    return chunk_.bytes.get() + position - chunk_.from;
+}
+
+    /* Loads a chunk from file, starting from the byte at position "from" */
+    void LoadChunk(size_t from);
+
+    struct Chunk {
+        size_t from;
+        size_t to;
+        std::unique_ptr<char[]> bytes;
+    };
+
+    FileReader file_;
+    size_t chunk_size_;
+    Chunk chunk_;
+};
