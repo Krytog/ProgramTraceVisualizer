ef146bcc9ce57995c18b7d5b4e7637ff4f77654c
Daniil Baranov <danya2003.db@gmail.com>
2024-04-14 20:33:45 +0300

    Update main for testing

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <GL/glew.h>
 
+#include <cstdint>
 #include <iostream>
 #include <GLFW/glfw3.h>
 #include <GUI/Window/Window.h>
@@ -11,8 +12,10 @@
 #include <Core/Plotting/Plot2DMesh/Plot2DMesh.h>
 #include <Core/Plotting/HilbertCurve/HilbertCurve.h>
 #include <Core/IPmoves/Clamping/DataClamper.h>
-#include <Core/IPmoves/SlidingWindow/SlidingWindowIterator.h>
+#include <Core/IPmoves/SlidingWindow/SlidingWindowIteratorExtended.h>
+#include <Core/IPmoves/IPmovesHandler/AddressBuffer.h>
 #include <Utils/FrameRater/FrameRater.h>
+#include <limits>
 #include <memory>
 
 int main(int argc, char** argv) {
@@ -24,30 +27,34 @@ int main(int argc, char** argv) {
 
     glEnable(GL_DEPTH_TEST);
 
-    const size_t cells = 1024;
+    const size_t cells = 128;
 
     std::shared_ptr<Plot2DMesh> plot = std::make_shared<Plot2DMesh>(cells);
 
     ui_manager.GetViewScene().AddObject(plot);
 
     LightTimer run_timer;
-    HilbertCurve2D hilbert_curve(10);
+    HilbertCurve2D hilbert_curve(7);
     std::cout << "Hilbert Curve took " << run_timer.EvaluateTime() << std::endl;
 
-    std::vector<int> values;
-    for (int i = 0; i < cells * cells; ++i) {
-        values.push_back(i);
-    }
-    std::shared_ptr<DataClamper<int>> data_clamper = std::make_shared<MinMaxDataClamper<int>>(values);
+    const uintptr_t lower = 100514797300000;
+    const uintptr_t higher = 100514798300000;
+    std::shared_ptr<DataClamper<uintptr_t>> data_clamper = std::make_shared<AbsoluteDataClamper<uintptr_t>>(lower, higher);
 
-    const size_t sliding_window_size = 1000;
-    SlidingWindowIterator sliding_window(values, sliding_window_size);
+    const std::string filename = "captured_ip.trace";
+    AddressBuffer buffer(filename);
+    buffer.SetMaxMemory(1<<15);
+
+    const size_t sliding_window_size = 1;
+    SlidingWindowIteratorExtended sliding_window(buffer);
+    sliding_window.SetSize(sliding_window_size);
+    sliding_window.Restart();
+    //sliding_window.SetProgress(0.98);
 
     run_timer.ResetTime();
     size_t frames_counter = 0;
     FrameRater<144> frame_rater;
 	while (!window.IsPendingClose()) {
-
         const auto size = window.GetWindowSize();
         glViewport(0, 0, size.first, size.second);
 
@@ -57,10 +64,8 @@ int main(int argc, char** argv) {
         std::vector<GLfloat> data;
 
         auto& detail_scene = ui_manager.GetDetailsScene();
-        if (!detail_scene.GetInnerBuffer().empty()) {
-            for (int i = 0; i < sliding_window_size; ++i) {
-                detail_scene.PopFrontLine();
-            }
+        while (!detail_scene.GetInnerBuffer().empty()) {
+            detail_scene.PopFrontLine();
         }
         
         auto [start, end] = *sliding_window;
@@ -69,16 +74,19 @@ int main(int argc, char** argv) {
             const auto point = hilbert_curve.Seq2XY(seq_number);
             data.push_back(2 * point.x - 1);
             data.push_back(2 * point.y - 1);
-            data.push_back(1 - static_cast<float>(cur - start + 1) / (end - start));
-            detail_scene.PushLine(std::to_string(*cur));
+            //data.push_back(1 - static_cast<float>(cur - start + 1) / (end - start));
+            //detail_scene.PushLine(std::to_string(*cur));
+            data.push_back(1);
         }
-        ++sliding_window;
+        sliding_window.Advance();
         if (!sliding_window) {
             sliding_window.Restart();
         }
 
+        detail_scene.PushLine(std::to_string(static_cast<float>(start - buffer.begin()) / (buffer.end() - buffer.begin())));
+
         plot->LoadData(data.data(), data.size() * sizeof(GLfloat));
-        
+
         ui_manager.DrawUI();
 
 		window.Render();
@@ -87,12 +95,10 @@ int main(int argc, char** argv) {
         ++frames_counter;
         if (run_timer.EvaluateTime() >= 0.5) {
             run_timer.ResetTime();
-            std::cout << frames_counter << std::endl;
             ui_manager.GetViewScene().SetFrametime(0.5 / frames_counter);
             frames_counter = 0;
         }
         frame_rater.Sleep();
 	}
-    std::cout << "IT TOOK " << run_timer.EvaluateTime() << std::endl;
 	return 0;
 }
