158da31bf653ad1d56865829ff24d09b6249be35
Daniil Baranov <danya2003.db@gmail.com>
2024-04-15 04:47:02 +0300

    Update UIManager

--- src/GUI/UI/UIManager/UIManager.cpp
+++ src/GUI/UI/UIManager/UIManager.cpp
@@ -8,85 +8,95 @@
 #define GLSL_VERSION "#version 330"
 
 namespace {
-	static const constexpr float kOptionsWidgetHeight = 35.0f;
+static const constexpr float kOptionsWidgetHeight = 35.0f;
 
-	static const constexpr float kViewSceneWidthCoef = 0.75f;
+static const constexpr float kViewSceneWidthCoef = 0.75f;
 
-	static const constexpr std::pair<float, float> GetViewScenePos(const std::pair<int, int>& window_size) {
-		return { (1.0f - kViewSceneWidthCoef) * window_size.first, kOptionsWidgetHeight };
-	}
-
-	static const constexpr std::pair<float, float> GetViewSceneSize(const std::pair<int, int>& window_size) {
-		const float height = window_size.second - kOptionsWidgetHeight;
-		const float width = kViewSceneWidthCoef * window_size.first;
-		return { width, height };
-	}
-
-	static const constexpr std::pair<float, float> GetDetailsScenePos(const std::pair<int, int>& window_size) {
-		return { 0.0f, kOptionsWidgetHeight };
-	}
-
-	static const constexpr std::pair<float, float> GetDetailsSceneSize(const std::pair<int, int>& window_size) {
-		const float height = window_size.second - kOptionsWidgetHeight;
-		const float width = (1.0f - kViewSceneWidthCoef) * window_size.first;
-		return { width, height };
-	}
-
-	static const constexpr std::pair<float, float> GetOptionsScenePos(const std::pair<int, int>& window_size) {
-		(void)window_size;
-		return { 0.0f, 0.0f };
-	}
-
-	static const constexpr std::pair<float, float> GetOptionsSceneSize(const std::pair<int, int>& window_size) {
-		const float height = kOptionsWidgetHeight;
-		const float width = window_size.first;
-		return { width, height };
-	}
+static const constexpr std::pair<float, float> GetViewScenePos(const std::pair<int, int>& window_size) {
+    return {(1.0f - kViewSceneWidthCoef) * window_size.first, kOptionsWidgetHeight};
 }
 
-UIManager::UIManager(GLFWwindow* glfw_window, const std::pair<int, int>& window_size): 
-	view_scene_(GetViewScenePos(window_size), GetViewSceneSize(window_size)),
-	details_scene_(GetDetailsScenePos(window_size), GetDetailsSceneSize(window_size)),
-	options_scene_(GetOptionsScenePos(window_size), GetOptionsSceneSize(window_size)) {
-	InitImGui(glfw_window);
+static const constexpr std::pair<float, float> GetViewSceneSize(const std::pair<int, int>& window_size) {
+    const float height = window_size.second - kOptionsWidgetHeight;
+    const float width = kViewSceneWidthCoef * window_size.first;
+    return {width, height};
+}
+
+static const constexpr std::pair<float, float> GetDetailsScenePos(const std::pair<int, int>& window_size) {
+    return {0.0f, kOptionsWidgetHeight};
+}
+
+static const constexpr std::pair<float, float> GetDetailsSceneSize(const std::pair<int, int>& window_size) {
+    const float height = window_size.second - kOptionsWidgetHeight;
+    const float width = (1.0f - kViewSceneWidthCoef) * window_size.first;
+    return {width, height};
+}
+
+static const constexpr std::pair<float, float> GetOptionsScenePos(const std::pair<int, int>& window_size) {
+    (void)window_size;
+    return {0.0f, 0.0f};
+}
+
+static const constexpr std::pair<float, float> GetOptionsSceneSize(const std::pair<int, int>& window_size) {
+    const float height = kOptionsWidgetHeight;
+    const float width = window_size.first;
+    return {width, height};
+}
+}  // namespace
+
+UIManager* UIManager::k_main_ui_manager = nullptr;
+
+UIManager::UIManager(GLFWwindow* glfw_window, const std::pair<int, int>& window_size)
+    : view_scene_(GetViewScenePos(window_size), GetViewSceneSize(window_size)),
+      details_scene_(GetDetailsScenePos(window_size), GetDetailsSceneSize(window_size)),
+      options_scene_(GetOptionsScenePos(window_size), GetOptionsSceneSize(window_size)) {
+    InitImGui(glfw_window);
 }
 
 void UIManager::DrawUI() const {
-	ImGui_ImplOpenGL3_NewFrame();
-	ImGui_ImplGlfw_NewFrame();
-	ImGui::NewFrame();
-	RenderAllScenes();
-	ImGui::Render();
-	ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+    ImGui_ImplOpenGL3_NewFrame();
+    ImGui_ImplGlfw_NewFrame();
+    ImGui::NewFrame();
+    RenderAllScenes();
+    ImGui::Render();
+    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 }
 
 void UIManager::InitImGui(GLFWwindow* glfw_window) const {
-	IMGUI_CHECKVERSION();
-	ImGui::CreateContext();
-	ImGui_ImplGlfw_InitForOpenGL(glfw_window, true);
-	ImGui_ImplOpenGL3_Init(GLSL_VERSION);
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGui_ImplGlfw_InitForOpenGL(glfw_window, true);
+    ImGui_ImplOpenGL3_Init(GLSL_VERSION);
 }
 
 UIManager::~UIManager() {
-	ImGui_ImplOpenGL3_Shutdown();
-	ImGui_ImplGlfw_Shutdown();
-	ImGui::DestroyContext();
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
 }
 
 ViewScene& UIManager::GetViewScene() {
-	return view_scene_;
+    return view_scene_;
 }
 
 DetailsScene& UIManager::GetDetailsScene() {
-	return details_scene_;
+    return details_scene_;
 }
 
 OptionsScene& UIManager::GetOptionsScene() {
-	return options_scene_;
+    return options_scene_;
 }
 
 void UIManager::RenderAllScenes() const {
-	view_scene_.Render();
-	details_scene_.Render();
-	options_scene_.Render();
+    view_scene_.Render();
+    details_scene_.Render();
+    options_scene_.Render();
+}
+
+void UIManager::MarkAsMain() {
+    k_main_ui_manager = this;
+}
+
+UIManager* UIManager::GetMainUIManager() {
+    return k_main_ui_manager;
 }
--- src/GUI/UI/UIManager/UIManager.h
+++ src/GUI/UI/UIManager/UIManager.h
@@ -8,30 +8,38 @@
 
 class UIManager {
 public:
-	UIManager(struct GLFWwindow* glfw_window, const std::pair<int, int>& size);
+    UIManager(struct GLFWwindow* glfw_window, const std::pair<int, int>& size);
 
-	~UIManager();
+    ~UIManager();
 
-	/* Draws all the UI into the render buffer */
-	void DrawUI() const;
+    /* Draws all the UI into the render buffer */
+    void DrawUI() const;
 
-	/* Returns the reference to the ViewScene of the UI */
-	ViewScene& GetViewScene();
+    /* Returns the reference to the ViewScene of the UI */
+    ViewScene& GetViewScene();
 
-	/* Returns the reference to the DetailsScene of the UI */
-	DetailsScene& GetDetailsScene();
+    /* Returns the reference to the DetailsScene of the UI */
+    DetailsScene& GetDetailsScene();
 
-	/* Returns the reference to the OptionsScene of the UI */
-	OptionsScene& GetOptionsScene();
+    /* Returns the reference to the OptionsScene of the UI */
+    OptionsScene& GetOptionsScene();
+
+    /* Returns a pointer to a UIManager that is marked as main */
+    static UIManager* GetMainUIManager();
+
+    /* Marks a UIManager as main */
+    void MarkAsMain();
 
 private:
-	/* Inits underlying ImGui library that is used for UI handling. Should be called in the c-tor */
-	void InitImGui(struct GLFWwindow* glfw_window) const;
+    /* Inits underlying ImGui library that is used for UI handling. Should be called in the c-tor */
+    void InitImGui(struct GLFWwindow* glfw_window) const;
 
-	/* Renders all the scenes of the UI */
-	void RenderAllScenes() const;
+    /* Renders all the scenes of the UI */
+    void RenderAllScenes() const;
 
-	ViewScene view_scene_;
-	DetailsScene details_scene_;
-	OptionsScene options_scene_;
+    ViewScene view_scene_;
+    DetailsScene details_scene_;
+    OptionsScene options_scene_;
+
+    static UIManager* k_main_ui_manager;
 };
