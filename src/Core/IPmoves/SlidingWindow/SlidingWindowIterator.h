9457281af2f9e13d9c943e0a09548a4ea0889b1f
Daniil Baranov <danya2003.db@gmail.com>
2024-04-15 03:26:46 +0300

    Extend SlidingWindows

--- src/Core/IPmoves/SlidingWindow/SlidingWindowIterator.h
+++ src/Core/IPmoves/SlidingWindow/SlidingWindowIterator.h
@@ -5,49 +5,49 @@
 
 template <typename Container>
 class SlidingWindowIterator {
-	using Iterator = typename Container::iterator;
+    using Iterator = typename Container::iterator;
 
 public:
-	SlidingWindowIterator(Container& data, size_t window_size): 
-		container_begin_(data.begin()),
-		container_end_(data.end()),
-		begin_(data.begin()),
-		window_size_(window_size)
-	{}
+    SlidingWindowIterator(Container& data, size_t window_size)
+        : container_begin_(data.begin()),
+          container_end_(data.end()),
+          begin_(data.begin()),
+          window_size_(window_size) {
+    }
 
-	[[nodiscard]] std::pair<Iterator, Iterator> operator*() const {
-		return { begin_, begin_ + window_size_ };
-	}
+    [[nodiscard]] std::pair<Iterator, Iterator> operator*() const {
+        return {begin_, begin_ + window_size_};
+    }
 
-	[[nodiscard]] operator bool() const {
-		return window_size_ <= container_end_ - begin_;
-	}
+    [[nodiscard]] operator bool() const {
+        return window_size_ <= container_end_ - begin_;
+    }
 
-	SlidingWindowIterator& operator+=(size_t offset) {
-		begin_ += offset;
-		return *this;
-	}
+    SlidingWindowIterator& operator+=(size_t offset) {
+        begin_ += offset;
+        return *this;
+    }
 
-	SlidingWindowIterator& operator-=(size_t offset) {
-		begin_ -= offset;
-		if (begin_ < container_begin_) {
-			begin_ = container_begin_;
-		}
-		return *this;
-	}
+    SlidingWindowIterator& operator-=(size_t offset) {
+        begin_ -= offset;
+        if (begin_ < container_begin_) {
+            begin_ = container_begin_;
+        }
+        return *this;
+    }
 
-	SlidingWindowIterator& operator++() {
-		++begin_;
-		return* this;
-	}
+    SlidingWindowIterator& operator++() {
+        ++begin_;
+        return *this;
+    }
 
-	void Restart() {
-		begin_ = container_begin_;
-	}
+    void Restart() {
+        begin_ = container_begin_;
+    }
 
 private:
-	const Iterator container_begin_;
-	const Iterator container_end_;
-	Iterator begin_;
-	size_t window_size_;
+    const Iterator container_begin_;
+    const Iterator container_end_;
+    Iterator begin_;
+    size_t window_size_;
 };
--- src/Core/IPmoves/SlidingWindow/SlidingWindowIteratorExtended.h
+++ src/Core/IPmoves/SlidingWindow/SlidingWindowIteratorExtended.h
@@ -5,36 +5,37 @@
 
 template <typename Container>
 class SlidingWindowIteratorExtended {
-	using Iterator = typename Container::iterator;
+    using Iterator = typename Container::iterator;
 
 public:
-	SlidingWindowIteratorExtended(Container& data): 
-		container_begin_(data.begin()),
-		container_size_(data.size()),
-		pos_(0),
-		window_size_(0)
-	{}
+    SlidingWindowIteratorExtended(Container& data)
+        : container_begin_(data.begin()), container_size_(data.size()), pos_(0), window_size_(0) {
+    }
 
-	[[nodiscard]] std::pair<Iterator, Iterator> operator*() const {
+    [[nodiscard]] std::pair<Iterator, Iterator> operator*() const {
         const Iterator begin = container_begin_ + GetBeginPos();
         const Iterator end = container_begin_ + GetEndPos();
-		return { begin, end };
-	}
+        return {begin, end};
+    }
 
-	[[nodiscard]] operator bool() const {
-		return pos_ < container_size_ + window_size_ - 1;
-	}
+    [[nodiscard]] operator bool() const {
+        return pos_ < container_size_ + window_size_ - 1;
+    }
 
-	void Advance() {
-		++pos_;
-	}
+    void Advance(size_t to_advance) {
+        pos_ += to_advance;
+    }
 
-	void Restart() {
-		pos_ = 0;
-	}
+    void Restart() {
+        pos_ = 0;
+    }
 
     void SetSize(size_t size) {
         window_size_ = size;
+    }
+
+    [[nodiscard]] size_t GetWindowSize() const {
+        return window_size_;
     }
 
     void SetPosition(size_t position) {
@@ -44,6 +45,24 @@ public:
     void SetProgress(float alpha) {
         const size_t position = alpha * (container_size_ + window_size_ - 1);
         SetPosition(position);
+    }
+
+    [[nodiscard]] float GetProgress() const {
+        return static_cast<float>(pos_) / (container_size_ + window_size_ - 1);
+    }
+
+    /* Returns an iterator to begin, an iterator to end, and an offset. A positive offset equals to the number
+    of element that should go before the window, a negative offset equals to the number of elements that
+    should go after the window */
+    [[nodiscard]] std::tuple<Iterator, Iterator, ptrdiff_t> GetWindowWithOffset() const {
+        ptrdiff_t offset = 0;
+        if (pos_ < window_size_) {
+            offset = window_size_ - 1 - pos_;
+        } else if (pos_ >= container_size_) {
+            offset = container_size_ - 1 - pos_;
+        }
+        auto [begin, end] = SlidingWindowIteratorExtended::operator*();
+        return {begin, end, offset};
     }
 
 private:
@@ -59,8 +78,8 @@ private:
         return std::min(container_size_, advanced_pos);
     }
 
-	const Iterator container_begin_;
-	const size_t container_size_;
+    const Iterator container_begin_;
+    const size_t container_size_;
     size_t pos_;
-	size_t window_size_;
+    size_t window_size_;
 };
