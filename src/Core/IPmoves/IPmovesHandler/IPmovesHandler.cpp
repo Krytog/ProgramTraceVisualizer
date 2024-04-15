b51002fd6b86e966022538c713e85ef94796e151
Daniil Baranov <danya2003.db@gmail.com>
2024-04-15 06:50:44 +0300

    Add all basic features to IPmovesHandler

--- src/Core/IPmoves/IPmovesHandler/IPmovesHandler.cpp
+++ src/Core/IPmoves/IPmovesHandler/IPmovesHandler.cpp
@@ -1,45 +1,54 @@
 #include "IPmovesHandler.h"
 
 #include <Core/Plotting/HilbertCurve/HilbertCurveManager.h>
+#include <sys/types.h>
 
 #include <cstdint>
 #include <limits>
+#include <memory>
 #include <thread>
 #include <mutex>
 #include <vector>
+#include <unordered_map>
+#include "Core/Plotting/Plot2DMesh/Plot2DMesh.h"
 
 namespace {
-    struct MinMaxWorkerArgs {
-        std::mutex* mutex;
-        FileReader* file;
-        size_t chunk_size;
-        size_t from;
-        size_t to;
-        uintptr_t* min;
-        uintptr_t* max;
-    };
+struct MinMaxWorkerArgs {
+    std::mutex* mutex;
+    FileReader* file;
+    size_t chunk_size;
+    size_t from;
+    size_t to;
+    uintptr_t* min;
+    uintptr_t* max;
+};
 
-    void MinMaxWorker(MinMaxWorkerArgs args) {
-        while (args.from < args.to) {
-            const size_t bytes_to_read = std::min(args.to - args.from, args.chunk_size);
-            args.mutex->lock();
-            auto [data_chunk, real_size] = args.file->GetChunk(args.from, bytes_to_read);
-            args.mutex->unlock();
-            size_t bytes_handled = 0;
-            for (size_t i = 0; i < real_size; i += sizeof(uintptr_t)) {
-                const uintptr_t address = *reinterpret_cast<uintptr_t*>(data_chunk.get() + i);
-                if (address < *args.min) {
-                    *args.min = address;
-                }
-                if (address > *args.max) {
-                    *args.max = address;
-                }
-                bytes_handled = (i + sizeof(uintptr_t));
+void MinMaxWorker(MinMaxWorkerArgs args) {
+    while (args.from < args.to) {
+        const size_t bytes_to_read = std::min(args.to - args.from, args.chunk_size);
+        args.mutex->lock();
+        auto [data_chunk, real_size] = args.file->GetChunk(args.from, bytes_to_read);
+        args.mutex->unlock();
+        size_t bytes_handled = 0;
+        for (size_t i = 0; i < real_size; i += sizeof(uintptr_t)) {
+            const uintptr_t address = *reinterpret_cast<uintptr_t*>(data_chunk.get() + i);
+            if (address < *args.min) {
+                *args.min = address;
             }
-            args.from += bytes_handled;
+            if (address > *args.max) {
+                *args.max = address;
+            }
+            bytes_handled = (i + sizeof(uintptr_t));
         }
+        args.from += bytes_handled;
     }
 }
+
+inline float GetTemperature(size_t pos, size_t total) {
+    return static_cast<float>(pos) / total;
+}
+
+}  // namespace
 
 void IPmovesHandler::FindAndSetMaxMin() {
     const size_t file_size = data_buffer_.GetRawFileReader()->GetSize();
@@ -53,28 +62,27 @@ void IPmovesHandler::FindAndSetMaxMin() {
     std::vector<uintptr_t> maxs(threads, std::numeric_limits<uintptr_t>::min());
     const size_t max_memory = data_buffer_.GetMaxMemory();
     for (int i = 0; i < threads - 1; ++i) {
-        workers.emplace_back(MinMaxWorkerArgs{
-            .mutex = &mutex,
-            .file = data_buffer_.GetRawFileReader(),
-            .chunk_size = max_memory,
-            .from = i * per_thread * sizeof(uintptr_t),
-            .to = (i + 1) * per_thread * sizeof(uintptr_t),
-            .min = mins.data() + i,
-            .max = maxs.data() + i
-        });
+        workers.emplace_back(MinMaxWorker, MinMaxWorkerArgs{.mutex = &mutex,
+                                                            .file = data_buffer_.GetRawFileReader(),
+                                                            .chunk_size = max_memory,
+                                                            .from = i * per_thread * sizeof(uintptr_t),
+                                                            .to = (i + 1) * per_thread * sizeof(uintptr_t),
+                                                            .min = mins.data() + i,
+                                                            .max = maxs.data() + i});
     }
-    workers.emplace_back(MinMaxWorkerArgs{
-        .mutex = &mutex,
-        .file = data_buffer_.GetRawFileReader(),
-        .chunk_size = max_memory,
-        .from = (threads - 1) * per_thread * sizeof(uintptr_t),
-        .to = file_size,
-        .min = mins.data() + (threads - 1),
-        .max = maxs.data() + (threads - 1)
-    });
+    workers.emplace_back(MinMaxWorker,
+                         MinMaxWorkerArgs{.mutex = &mutex,
+                                          .file = data_buffer_.GetRawFileReader(),
+                                          .chunk_size = max_memory,
+                                          .from = (threads - 1) * per_thread * sizeof(uintptr_t),
+                                          .to = file_size,
+                                          .min = mins.data() + (threads - 1),
+                                          .max = maxs.data() + (threads - 1)});
     for (auto& worker : workers) {
         worker.join();
     }
+    min_address_ = std::numeric_limits<uintptr_t>::max();
+    max_address_ = std::numeric_limits<uintptr_t>::min();
     for (int i = 0; i < threads; ++i) {
         if (min_address_ > mins[i]) {
             min_address_ = mins[i];
@@ -102,10 +110,100 @@ float IPmovesHandler::GetProgress() const {
 }
 
 void IPmovesHandler::SetHilbertDegree(unsigned degree) {
+    if (hilbert_degree_ == degree) {
+        return;
+    }
     hilbert_degree_ = degree;
     hilbert_curve_ = HilbertCurveManager::GetHilbertCurve(degree);
+    const size_t cells = 1ull << degree;
+    plot_ = std::make_shared<Plot2DMesh>(cells);
+    cells_ = cells;
 }
 
 unsigned IPmovesHandler::GetHilbertDegree() const {
     return hilbert_degree_;
+}
+
+void IPmovesHandler::SetAdvanceCount(size_t count) {
+    advance_count_ = count;
+}
+
+size_t IPmovesHandler::GetAdvanceCount() const {
+    return advance_count_;
+}
+
+void IPmovesHandler::SetWindowSize(size_t window_size) {
+    sliding_window_.SetSize(window_size);
+}
+
+size_t IPmovesHandler::GetSlidingWindowSize() const {
+    return sliding_window_.GetWindowSize();
+}
+
+std::shared_ptr<Plot2DMesh> IPmovesHandler::GetPlot() const {
+    return plot_;
+}
+
+IPmovesHandler::IPmovesHandler(const std::string& filename)
+    : data_buffer_(filename), sliding_window_(data_buffer_) {
+    const unsigned basic_hilbert_degree = 10;
+    SetHilbertDegree(basic_hilbert_degree);
+
+    const size_t basic_max_memory = 1ull << 20;
+    SetMaxMemory(basic_max_memory);
+
+    FindAndSetMaxMin();
+    data_clamper_.SetBounds(min_address_, max_address_);
+
+    const size_t basic_advance_count = 1;
+    SetAdvanceCount(basic_advance_count);
+}
+
+std::vector<std::pair<uintptr_t, float>> IPmovesHandler::GetDataWithTemperatures() const {
+    auto [begin, end, offset] = sliding_window_.GetWindowWithOffset();
+    const size_t before_begin = std::max(offset, static_cast<ptrdiff_t>(0));
+    std::vector<std::pair<uintptr_t, float>> output;
+    output.reserve(sliding_window_.GetWindowSize());
+    for (auto iter = begin; iter != end; ++iter) {
+        const size_t pos = before_begin + (iter - begin);
+        const float temperature = GetTemperature(pos, sliding_window_.GetWindowSize() - 1);
+        const uintptr_t address = *iter;
+        output.emplace_back(address, temperature);
+    }
+    return output;
+}
+
+std::vector<float> IPmovesHandler::GetPreparedDataForRendering(
+    const std::vector<std::pair<uintptr_t, float>>& data_with_temperatures) const {
+    std::unordered_map<size_t, float> cells_with_temperature;
+    for (auto [address, temperature] : data_with_temperatures) {
+        const float clamped = data_clamper_.GetClamped(address);
+        const size_t seq_num = clamped * (cells_ * cells_ - 1);
+        if (cells_with_temperature[seq_num] < temperature) {
+            cells_with_temperature[seq_num] = temperature;
+        }
+    }
+    std::vector<float> output;
+    output.reserve(cells_with_temperature.size() * 3);
+    for (auto [seq_num, temperature] : cells_with_temperature) {
+        const auto point = hilbert_curve_->Seq2XY(seq_num);
+        output.push_back(2 * point.x - 1);
+        output.push_back(2 * point.y - 1);
+        output.push_back(temperature);
+    }
+    return output;
+}
+
+void IPmovesHandler::LoadDataForRendering() const {
+    const auto data_with_temperatures = GetDataWithTemperatures();
+    const auto ready_data = GetPreparedDataForRendering(data_with_temperatures);
+    plot_->LoadData(ready_data.data(), ready_data.size() * sizeof(GLfloat));
+}
+
+void IPmovesHandler::Update() {
+    if (!sliding_window_) {
+        sliding_window_.Restart();
+    }
+    LoadDataForRendering();
+    sliding_window_.Advance(advance_count_);
 }
--- src/Core/IPmoves/IPmovesHandler/IPmovesHandler.h
+++ src/Core/IPmoves/IPmovesHandler/IPmovesHandler.h
@@ -18,21 +18,53 @@ public:
     /* Returns the max amount of memory that can be used for caching */
     [[nodiscard]] size_t GetMaxMemory() const;
 
+    /* Sets the sliding window size */
+    void SetWindowSize(size_t window_size);
+
+    [[nodiscard]] size_t GetSlidingWindowSize() const;
+
     /* Sets current progress (relative position of the sliding window in the file) */
     void SetProgress(float alpha);
 
-    /* Returns current progress (relative position of the sliding window in the file)*/
+    /* Returns current progress (relative position of the sliding window in the file) */
     [[nodiscard]] float GetProgress() const;
 
-    /* Sets the Hilbert Curve degree. If new_degree != old_degree, then a corresponding curve is aquired and prepared for usage */
+    /* Sets the Hilbert Curve degree. If new_degree != old_degree, then a new curve and plot are created.
+     * Before doing that, old plot must be removed from any rendering system, and such a removal is user's
+     * duty */
     void SetHilbertDegree(unsigned degree);
 
     /* Returns the degree of the current Hilbert Curve */
     [[nodiscard]] unsigned GetHilbertDegree() const;
 
+    /* Sets the number by which the sliding window moves each update */
+    void SetAdvanceCount(size_t count);
+
+    /* Returns the number by which the sliding window moves each update */
+    [[nodiscard]] size_t GetAdvanceCount() const;
+
+    /* Returns a shared_ptr to the current plot */
+    [[nodiscard]] std::shared_ptr<Plot2DMesh> GetPlot() const;
+
+    /* Updates the IPMH for the next frame. In particular, this method moves the sliding window, prepares data
+       and loads it into mesh_plot */
+    void Update();
+
 private:
     /* Goes through the entire file and sets max and min addresses */
     void FindAndSetMaxMin();
+
+    /* Returns data from the sliding window with calculated temperatures. The newer an address is, the higher
+     * its temperature is */
+    [[nodiscard]] std::vector<std::pair<uintptr_t, float>> GetDataWithTemperatures() const;
+
+    /* Returns data in a format ready to be loaded into PlotMesh. Takes data with prepared temperatures */
+    [[nodiscard]] std::vector<float> GetPreparedDataForRendering(
+        const std::vector<std::pair<uintptr_t, float>>& data_with_temperatures) const;
+
+    /* Reads, prepares and loads data from the sliding window into the PlotMesh. Should be called exactly once
+     * per update */
+    void LoadDataForRendering() const;
 
     /* Data from file */
     AddressBuffer data_buffer_;
@@ -47,4 +79,6 @@ private:
 
     /* Parameters */
     unsigned hilbert_degree_;
+    size_t cells_;
+    size_t advance_count_;
 };
