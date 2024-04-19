1fb565a13580fd4c819abbb053330d1b8f9c6736
Daniil Baranov <danya2003.db@gmail.com>
2024-04-19 21:38:18 +0300

    Extend IPmovesControlScene

--- src/GUI/UI/Scenes/IPmovesControlScene/IPmovesControlScene.cpp
+++ src/GUI/UI/Scenes/IPmovesControlScene/IPmovesControlScene.cpp
@@ -2,19 +2,36 @@
 
 #include <imgui.h>
 #include <UI/Helpers/Alignment.h>
+#include <cmath>
 
 #define IPmovesControlScene_InnerName "IPmovesControl"
 
-IPmovesControlScene::IPmovesControlScene(const std::pair<float, float>& position, const std::pair<float, float>& size) :
-	BasicScene(position, size, IPmovesControlScene_InnerName) {}
+IPmovesControlScene::IPmovesControlScene(const std::pair<float, float>& position,
+                                         const std::pair<float, float>& size)
+    : BasicScene(position, size, IPmovesControlScene_InnerName) {
+}
 
 void IPmovesControlScene::RenderInner() const {
-	static float f1 = 12.3f;
     ALIGNMENT_AT_BEGIN(ImGui::Button("Pause"));
-	ImGui::SameLine();
-    ALIGNMENT_AT_CENTER(ImGui::SliderFloat("##Progress", &f1, 0.0f, 100.0f, "%.2f%%"));
+    ImGui::SameLine();
+    ALIGNMENT_AT_CENTER(ImGui::SliderFloat("##Progress", &cur_value_, 0.0f, 100.0f, "%.2f%%"));
     ImGui::SameLine();
     ALIGNMENT_AT_END(ImGui::Button("Settings"));
     ImGui::SameLine();
-    //ImGui::ShowDemoWindow();
+    // ImGui::ShowDemoWindow();
+}
+
+std::pair<bool, float> IPmovesControlScene::GetUserInput() {
+    const constexpr float kPrecision = 1e-6;
+    if (std::abs(cur_value_ - prev_value_) < kPrecision) {
+        return {false, cur_value_ / 100};
+    }
+    prev_value_ = cur_value_;
+    return {true, cur_value_ / 100};
+}
+
+void IPmovesControlScene::SetProgressValue(float progress) {
+    const float progress_in_percents = 100 * progress;
+    cur_value_ = progress_in_percents;
+    prev_value_ = progress_in_percents;
 }
--- src/GUI/UI/Scenes/IPmovesControlScene/IPmovesControlScene.h
+++ src/GUI/UI/Scenes/IPmovesControlScene/IPmovesControlScene.h
@@ -1,11 +1,22 @@
 #pragma once
 
 #include <UI/Scenes/BasicScene/BasicScene.h>
+#include <utility>
 
 class IPmovesControlScene : public BasicScene {
 public:
-	IPmovesControlScene(const std::pair<float, float>& position, const std::pair<float, float>& size);
+    IPmovesControlScene(const std::pair<float, float>& position, const std::pair<float, float>& size);
+
+    /* Sets a progress value to the slider */
+    void SetProgressValue(float progress);
+
+    /* Returns a pair of <bool, float>. If the bool == true, then the float is new input from the user.
+    Otherwise, the user hasn't inputed anything yet. */
+    std::pair<bool, float> GetUserInput();
 
 private:
-	void RenderInner() const override;
+    void RenderInner() const override;
+
+    mutable float cur_value_{0};
+    float prev_value_{0};
 };
