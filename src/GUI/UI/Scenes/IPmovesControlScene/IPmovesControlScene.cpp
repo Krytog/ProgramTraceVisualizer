1abed5630cb9e27b6277b62c1476e8723f152381
Daniil Baranov <danya2003.db@gmail.com>
2024-04-20 04:37:15 +0300

    Add IPmovesControlScene

--- src/GUI/UI/Scenes/IPmovesControlScene/IPmovesControlScene.cpp
+++ src/GUI/UI/Scenes/IPmovesControlScene/IPmovesControlScene.cpp
@@ -3,35 +3,119 @@
 #include <imgui.h>
 #include <UI/Helpers/Alignment.h>
 #include <cmath>
+#include "UI/Helpers/ValueWithPrev.h"
 
 #define IPmovesControlScene_InnerName "IPmovesControl"
 
+namespace {
+const constexpr ImGuiColorEditFlags kColorEditFlags =
+    ImGuiColorEditFlags_NoInputs | ImGuiColorEditFlags_AlphaBar | ImGuiColorEditFlags_AlphaPreview;
+}
+
 IPmovesControlScene::IPmovesControlScene(const std::pair<float, float>& position,
                                          const std::pair<float, float>& size)
-    : BasicScene(position, size, IPmovesControlScene_InnerName) {
+    : BasicScene(position, size, IPmovesControlScene_InnerName),
+      progress_(0),
+      pause_(false),
+      new_color_({1.0f, 0.0f, 0.0f, 1.0f}),
+      old_color_({0.0f, 0.0f, 1.0f, 1.0f}),
+      sliding_window_size_(10),
+      advance_(1),
+      hilbert_degree_(5),
+      max_memory_(1) {
 }
 
 void IPmovesControlScene::RenderInner() const {
-    ALIGNMENT_AT_BEGIN(ImGui::Button("Pause"));
+    PauseButton();
     ImGui::SameLine();
-    ALIGNMENT_AT_CENTER(ImGui::SliderFloat("##Progress", &cur_value_, 0.0f, 100.0f, "%.2f%%"));
+    ProgressSlider();
     ImGui::SameLine();
-    ALIGNMENT_AT_END(ImGui::Button("Settings"));
-    ImGui::SameLine();
+    SettingsButton();
     // ImGui::ShowDemoWindow();
 }
 
-std::pair<bool, float> IPmovesControlScene::GetUserInput() {
-    const constexpr float kPrecision = 1e-6;
-    if (std::abs(cur_value_ - prev_value_) < kPrecision) {
-        return {false, cur_value_ / 100};
-    }
-    prev_value_ = cur_value_;
-    return {true, cur_value_ / 100};
+std::pair<bool, float> IPmovesControlScene::GetInputProgress() {
+    auto progress_pair = GetValueAndChange(progress_);
+    progress_pair.second /= 100;  // because the user inputs it in percents
+    return progress_pair;
 }
 
 void IPmovesControlScene::SetProgressValue(float progress) {
     const float progress_in_percents = 100 * progress;
-    cur_value_ = progress_in_percents;
-    prev_value_ = progress_in_percents;
+    progress_.cur_value_ = progress_in_percents;
+    progress_.prev_value_ = progress_in_percents;
+}
+
+std::pair<bool, bool> IPmovesControlScene::GetInputPause() {
+    return GetValueAndChange(pause_);
+}
+
+std::pair<bool, float[4]> IPmovesControlScene::GetInputNewColor() {
+    return GetValueAndChange(new_color_);
+}
+
+std::pair<bool, float[4]> IPmovesControlScene::GetInputOldColor() {
+    return GetValueAndChange(old_color_);
+}
+
+std::pair<bool, int> IPmovesControlScene::GetInputSlidingWindowSize() {
+    return GetValueAndChange(sliding_window_size_);
+}
+
+std::pair<bool, int> IPmovesControlScene::GetInputAdvance() {
+    return GetValueAndChange(advance_);
+}
+
+std::pair<bool, int> IPmovesControlScene::GetInputHilbertDegree() {
+    return GetValueAndChange(hilbert_degree_);
+}
+
+std::pair<bool, size_t> IPmovesControlScene::GetInputMaxMemory() {
+    auto max_memory_raw = GetValueAndChange(max_memory_);
+    std::pair<bool, size_t> output;
+    output.first = max_memory_raw.first;
+    output.second = (1ull << 20) * max_memory_raw.second;  // because the user inputs it in megabytes
+    return output;
+}
+
+void IPmovesControlScene::PauseButton() const {
+    static const char* pause_text = "Pause";
+    static const char* resume_text = "Resume";
+    static auto get_button_text = [=, this] { return pause_.cur_value_ ? resume_text : pause_text; };
+    if (ALIGNMENT_AT_BEGIN(ImGui::Button(get_button_text()))) {
+        pause_.cur_value_ = !pause_.cur_value_;
+    }
+}
+
+void IPmovesControlScene::SettingsButton() const {
+    if (ALIGNMENT_AT_END(ImGui::Button("Settings"))) {
+        ImGui::OpenPopup("IPmoves Settings");
+    }
+    if (ImGui::BeginPopup("IPmoves Settings")) {
+        ImGui::Text("Size of sliding window: ");
+        ImGui::InputInt("##Size of sliding window", &sliding_window_size_.cur_value_);
+
+        ImGui::Text("Advance of sliding window: ");
+        ImGui::InputInt("##Advance of sliding window", &advance_.cur_value_);
+
+        ImGui::Text("Degree of Hilbert Curve: ");
+        ImGui::SliderInt("##Degree of Hilbert Curve", &hilbert_degree_.cur_value_, 1, 10);
+
+        ImGui::Text("Max memory for caching: ");
+        ImGui::InputInt("##Max memory for caching", &max_memory_.cur_value_);
+
+        ImGui::Text("Color of the newest element: ");
+        ImGui::SameLine();
+        ImGui::ColorEdit4("##Rightmost color", new_color_.cur_value_, kColorEditFlags);
+
+        ImGui::Text("Color of the oldest element: ");
+        ImGui::SameLine();
+        ImGui::ColorEdit4("##Leftmost color", old_color_.cur_value_, kColorEditFlags);
+
+        ImGui::EndPopup();
+    }
+}
+
+void IPmovesControlScene::ProgressSlider() const {
+    ALIGNMENT_AT_CENTER(ImGui::SliderFloat("##Progress", &progress_.cur_value_, 0.0f, 100.0f, "%.2f%%"));
 }
--- src/GUI/UI/Scenes/IPmovesControlScene/IPmovesControlScene.h
+++ src/GUI/UI/Scenes/IPmovesControlScene/IPmovesControlScene.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #include <UI/Scenes/BasicScene/BasicScene.h>
-#include <utility>
+#include <UI/Helpers/ValueWithPrev.h>
 
 class IPmovesControlScene : public BasicScene {
 public:
@@ -10,13 +10,35 @@ public:
     /* Sets a progress value to the slider */
     void SetProgressValue(float progress);
 
-    /* Returns a pair of <bool, float>. If the bool == true, then the float is new input from the user.
-    Otherwise, the user hasn't inputed anything yet. */
-    std::pair<bool, float> GetUserInput();
+    /* The following fuctions work as follows: they return a pair of <bool, T>. If the bool == true, then the
+    T is new input from the user. Otherwise, the user hasn't inputed anything new (from the last time this
+    function was called) yet. */
+
+    [[nodiscard]] std::pair<bool, float> GetInputProgress();
+    [[nodiscard]] std::pair<bool, bool> GetInputPause();
+    [[nodiscard]] std::pair<bool, float[4]> GetInputNewColor();
+    [[nodiscard]] std::pair<bool, float[4]> GetInputOldColor();
+    [[nodiscard]] std::pair<bool, int> GetInputSlidingWindowSize();
+    [[nodiscard]] std::pair<bool, int> GetInputAdvance();
+    [[nodiscard]] std::pair<bool, int> GetInputHilbertDegree();
+    [[nodiscard]] std::pair<bool, size_t> GetInputMaxMemory();
 
 private:
     void RenderInner() const override;
 
-    mutable float cur_value_{0};
-    float prev_value_{0};
+    /* The following three functions mark up the widget and perform layout logic */
+    void PauseButton() const;
+    void ProgressSlider() const;
+    void SettingsButton() const;
+
+    /* A lot of mutable variables here as Render of IRenderable is const, but DearImGui rendering and state
+     * change always come together */
+    mutable ValueWithPrev<float> progress_;
+    mutable ValueWithPrev<bool> pause_;
+    mutable ValueWithPrev<float[4]> new_color_;
+    mutable ValueWithPrev<float[4]> old_color_;
+    mutable ValueWithPrev<int> sliding_window_size_;
+    mutable ValueWithPrev<int> advance_;
+    mutable ValueWithPrev<int> hilbert_degree_;
+    mutable ValueWithPrev<int> max_memory_;
 };
