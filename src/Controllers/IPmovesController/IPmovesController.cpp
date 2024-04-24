23dfcb75eb35251da09ad2a4f345a8bda83dce18
Daniil Baranov <danya2003.db@gmail.com>
2024-04-24 18:21:31 +0300

    Adjust IPmovesHandler controls

--- src/Controllers/IPmovesController/IPmovesController.cpp
+++ src/Controllers/IPmovesController/IPmovesController.cpp
@@ -3,65 +3,105 @@
 #include <Core/IPmoves/IPmovesHandler/IPmovesHandler.h>
 #include <GUI/UI/UIManager/UIManager.h>
 
+#include <sstream>
+#include "UI/Scenes/IPmovesControlScene/IPmovesControlScene.h"
+
+namespace {
+template <typename T>
+inline std::string GetNumberAsHex(T number) {
+    std::stringstream sstream;
+    sstream << "0x" << std::hex << number;
+    return sstream.str();
+}
+
+inline void UpdateDetails(UIManager* ui_manager, IPmovesHandler* handler) {
+    auto& details = ui_manager->GetDetailsScene();
+    while (!details.GetInnerBuffer().empty()) {
+        details.PopBackLine();
+    }
+    for (const auto& address : handler->GetCurrentAddresses()) {
+        const std::string hex_address = GetNumberAsHex(address);
+        details.PushLine(hex_address);
+    }
+}
+
+inline void SynchronizeProgress(IPmovesHandler* handler, IPmovesControlScene& controls) {
+    const auto [has_new_input, input] = controls.GetInputProgress();
+    if (has_new_input) {
+        handler->SetProgress(input);
+    } else {
+        const float progress = handler->GetProgress();
+        controls.SetProgressValue(progress);
+    }
+}
+
+inline void SynchronizePuase(IPmovesHandler* handler, IPmovesControlScene& controls) {
+    const auto [has_new_input, is_paused] = controls.GetInputPause();
+    if (has_new_input) {
+        if (is_paused) {
+            handler->Pause();
+        } else {
+            handler->Unpause();
+        }
+    }
+}
+
+inline void SynchronizeNewColor(IPmovesHandler* handler, IPmovesControlScene& controls) {
+    const auto [has_new_input, new_color] = controls.GetInputNewColor();
+    if (has_new_input) {
+        handler->SetColorForNewest(new_color);
+    }
+}
+
+inline void SynchronizeOldColor(IPmovesHandler* handler, IPmovesControlScene& controls) {
+    const auto [has_new_input, old_color] = controls.GetInputOldColor();
+    if (has_new_input) {
+        handler->SetColorForOldest(old_color);
+    }
+}
+
+inline void SynchronizeSlidingWindowSize(IPmovesHandler* handler, IPmovesControlScene& controls) {
+    const auto [has_new_input, sliding_window_size] = controls.GetInputSlidingWindowSize();
+    if (has_new_input) {
+        handler->SetWindowSize(sliding_window_size);
+    }
+}
+
+inline void SynchronizeAdvance(IPmovesHandler* handler, IPmovesControlScene& controls) {
+    const auto [has_new_input, advance] = controls.GetInputAdvance();
+    if (has_new_input) {
+        handler->SetAdvanceCount(advance);
+    }
+}
+
+inline void SynchronizeHilbertDegree(IPmovesHandler* handler, IPmovesControlScene& controls) {
+    const auto [has_new_input, hilbert_degree] = controls.GetInputHilbertDegree();
+    if (has_new_input) {
+        handler->SetHilbertDegree(hilbert_degree);
+    }
+}
+
+inline void SynchronizeMaxMemory(IPmovesHandler* handler, IPmovesControlScene& controls) {
+    const auto [has_new_input, max_memory] = controls.GetInputMaxMemory();
+    if (has_new_input) {
+        handler->SetMaxMemory(max_memory);
+    }
+}
+}  // namespace
+
 namespace controllers::ipmoves {
 
 void Synchronize(UIManager* ui_manager, IPmovesHandler* handler) {
     auto& controls = ui_manager->GetIPmovesControlScene();
-    {  // progress
-        const auto [has_new_input, input] = controls.GetInputProgress();
-        if (has_new_input) {
-            handler->SetProgress(input);
-        } else {
-            const float progress = handler->GetProgress();
-            controls.SetProgressValue(progress);
-        }
-    }
-    {  // pause
-        const auto [has_new_input, is_paused] = controls.GetInputPause();
-        if (has_new_input) {
-            if (is_paused) {
-                handler->Pause();
-            } else {
-                handler->Unpause();
-            }
-        }
-    }
-    {  // new_color
-        const auto [has_new_input, new_color] = controls.GetInputNewColor();
-        if (has_new_input) {
-            handler->SetColorForNewest(new_color);
-        }
-    }
-    {  // old_color
-        const auto [has_new_input, old_color] = controls.GetInputOldColor();
-        if (has_new_input) {
-            handler->SetColorForOldest(old_color);
-        }
-    }
-    {  // sliding_window_size
-        const auto [has_new_input, sliding_window_size] = controls.GetInputSlidingWindowSize();
-        if (has_new_input) {
-            handler->SetWindowSize(sliding_window_size);
-        }
-    }
-    {  // advance
-        const auto [has_new_input, advance] = controls.GetInputAdvance();
-        if (has_new_input) {
-            handler->SetAdvanceCount(advance);
-        }
-    }
-    {  // hilbert_degree
-        const auto [has_new_input, hilbert_degree] = controls.GetInputHilbertDegree();
-        if (has_new_input) {
-            handler->SetHilbertDegree(hilbert_degree);
-        }
-    }
-    {  // max_memory
-        const auto [has_new_input, max_memory] = controls.GetInputMaxMemory();
-        if (has_new_input) {
-            handler->SetMaxMemory(max_memory);
-        }
-    }
+    SynchronizeProgress(handler, controls);
+    SynchronizePuase(handler, controls);
+    SynchronizeNewColor(handler, controls);
+    SynchronizeOldColor(handler, controls);
+    SynchronizeSlidingWindowSize(handler, controls);
+    SynchronizeAdvance(handler, controls);
+    SynchronizeHilbertDegree(handler, controls);
+    SynchronizeMaxMemory(handler, controls);
+    UpdateDetails(ui_manager, handler);
 }
 
 std::unique_ptr<IPmovesHandler> Initialize(UIManager* ui_manager, const std::string& filename) {
@@ -70,7 +110,6 @@ std::unique_ptr<IPmovesHandler> Initialize(UIManager* ui_manager, const std::str
     static const constexpr float kInitProgress = 0.0f;
     controls.SetProgressValue(kInitProgress);
     const auto state = controls.GetState();
-    handler->SetProgress(state.progress);
     handler->SetAdvanceCount(state.advance);
     handler->SetWindowSize(state.sliding_window_size);
     handler->SetHilbertDegree(state.hilbert_degree);
@@ -83,6 +122,7 @@ std::unique_ptr<IPmovesHandler> Initialize(UIManager* ui_manager, const std::str
         handler->Unpause();
     }
     ui_manager->GetViewScene().AddObject(handler->GetPlot());
+    handler->SetProgress(state.progress);
     return handler;
 }
 
--- src/Controllers/IPmovesController/IPmovesController.h
+++ src/Controllers/IPmovesController/IPmovesController.h
@@ -7,10 +7,14 @@ class UIManager;       // Forward-declaration
 class IPmovesHandler;  // Forward-declaration
 
 namespace controllers::ipmoves {
+/* Returns a new IPmovesHandler, initialized with args from UIManager and binded to the file named filename */
 std::unique_ptr<IPmovesHandler> Initialize(UIManager* ui_manager, const std::string& filename);
 
+/* Synchronizes states of UIManager and IPmovesHandler. In particular, it applies the user's input to
+ * IPmovesHandler */
 void Synchronize(UIManager* ui_manager, IPmovesHandler* handler);
 
+/* Sets all the needed callbacks that will be executed when the user does some input */
 void SetCallbacks(std::unique_ptr<IPmovesHandler>* handler_keeper, UIManager* ui_manager);
 
 }  // namespace controllers::ipmoves
