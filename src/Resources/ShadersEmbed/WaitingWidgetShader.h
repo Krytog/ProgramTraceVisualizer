5856ada7d682240a418a2dc7d6a5b7fd876b828a
Daniil Baranov <danya2003.db@gmail.com>
2024-05-05 20:53:53 +0300

    Change waiting shader

--- src/Resources/ShadersEmbed/WaitingWidgetShader.h
+++ src/Resources/ShadersEmbed/WaitingWidgetShader.h
@@ -1,6 +1,6 @@
 #pragma once
 
-// From: https://www.shadertoy.com/view/3tdSRn
+// From: https://www.shadertoy.com/view/sscyDB
 
 static const constexpr char* kWaitingWidgetShaderFrag = R"(
 #version 330 core
@@ -11,35 +11,27 @@ uniform float height;
 
 out vec4 final_color;
 
-vec3 drawCircle(vec2 pos, float radius, float width, float power, vec4 color) {
-    float dist1 = length(pos);
-    dist1 = fract((dist1 * 5.0) - fract(time));
-    float dist2 = dist1 - radius;
-    float intensity = pow(radius / abs(dist2), width); 
-    vec3 col = color.rgb * intensity * power * max((0.8- abs(dist2)), 0.0);
-    return col;
-}
+const float PI = 3.14159;
 
-vec3 hsv2rgb(float h, float s, float v) {
-    vec4 t = vec4(1.0, 2.0/3.0, 1.0/3.0, 3.0);
-    vec3 p = abs(fract(vec3(h) + t.xyz) * 6.0 - vec3(t.w));
-    return v * mix(vec3(t.x), clamp(p - vec3(t.x), 0.0, 1.0), s);
-}
-
-void mainImage( out vec4 fragColor, in vec2 fragCoord ) {
-    vec2 pos = (fragCoord.xy * 2.0 - vec2(width, height)) / min(width, height);
-    
-    float h = mix(0.5, 0.65, length(pos));
-    vec4 color = vec4(hsv2rgb(h, 1.0, 1.0), 1.0);
-    float radius = 0.5;
-    float _width = 0.8;
-    float power = 0.1;
-    vec3 finalColor = drawCircle(pos, radius, _width, power, color);
-
-    pos = abs(pos);
-    // vec3 finalColor = vec3(pos.x, 0.0, pos.y);
-
-    fragColor = vec4(finalColor, 1.0);
+void mainImage(out vec4 fragColor, in vec2 fragCoord)
+{
+    vec2 uv = fragCoord / vec2(width, height);
+    float d = 0.0;
+    int n = 9;
+    vec3 col = vec3(0.);
+    for (int i = 0; i < n; i++) {
+        float findex = float(i) / float(n);
+        float speed = 1.0 + 1.0 - findex;
+        vec2 uv2 = vec2(uv.x, min(1., max(0., uv.y + 0.1 * sin(uv.x * 2. * PI + time * speed + findex * 10.1))));
+        float offset = 3. * findex * PI + 0.3 * time * (0.2 + findex * 0.3);
+        float f = 0.5 * (1. + sin(offset));
+        float d = abs(f - uv2.y);
+        float fcol = pow(1. - pow(d, 0.4), 3.5 + 4. * 0.5 * (1. + sin(time * (1. + findex) + 4. * findex * PI)));
+        float r = 360. * findex * fcol;
+        float b = i % 2 == 1 ? fcol * 0.9 : 0.;
+        col += vec3(b, fcol * findex, fcol);
+    }
+    fragColor = vec4( col, 1.0 );
 }
 
 void main() {
