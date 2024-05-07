c9c1ca8906896d93c2d20eb3918980c6b6a4cde2
Daniil Baranov <danya2003.db@gmail.com>
2024-05-07 19:28:20 +0300

    Update w2v controller

--- src/Controllers/W2VController/W2VController.cpp
+++ src/Controllers/W2VController/W2VController.cpp
@@ -4,6 +4,7 @@
 #include <GUI/UI/UIManager/UIManager.h>
 #include "Graphics/Primitives/IRenderable.h"
 #include "UI/Scenes/DetailsScene/DetailsScene.h"
+#include "UI/Scenes/W2VControlScene/W2VControlScene.h"
 
 #include <memory>
 
@@ -50,32 +51,7 @@ void UpdateUmapEmbeddingStatus(DetailsScene* details, const W2VHandler* handler)
     }
 }
 
-static const IRenderable* prev_plot = nullptr;
-static bool is_first_time_ready = true;
-
-}  // namespace
-
-namespace controllers::w2vhandler {
-
-void Synchronize(UIManager* ui_manager, W2VHandler* handler) {
-    handler->Update();
-    if (handler->IsReady()) {
-        if (is_first_time_ready) {
-            is_first_time_ready = false;
-            ui_manager->GetViewScene().RemoveObject(prev_plot);
-            ui_manager->GetViewScene().AddObject(handler->GetPlot());
-        }
-        // apply input;
-    } else {
-        if (!is_first_time_ready) {
-            ui_manager->GetViewScene().RemoveObject(prev_plot);
-            ui_manager->GetViewScene().AddObject(handler->GetPlot());
-            is_first_time_ready = true;
-        }
-        const auto [width, height] = ui_manager->GetViewScene().GetViewPortSize();
-        handler->SetProgressParams(width, height);
-    }
-    prev_plot = handler->GetPlot();
+void SynchronizeDetails(UIManager* ui_manager, W2VHandler* handler) {
     DetailsScene* details = &ui_manager->GetDetailsScene();
     ClearDetails(details);
     UpdateParsingProgress(details, handler);
@@ -89,6 +65,64 @@ void Synchronize(UIManager* ui_manager, W2VHandler* handler) {
         return;
     }
     UpdateUmapEmbeddingStatus(details, handler);
+}
+
+static const IRenderable* prev_plot = nullptr;
+static bool is_first_time_ready = true;
+
+void SynchronizePlot(UIManager* ui_manager, W2VHandler* handler) {
+    if (handler->IsReady()) {
+        if (is_first_time_ready) {
+            is_first_time_ready = false;
+            ui_manager->GetViewScene().RemoveObject(prev_plot);
+            ui_manager->GetViewScene().AddObject(handler->GetPlot());
+            ui_manager->GetW2VControlScene().SetRealDimension(handler->GetDimension());
+            ui_manager->GetW2VControlScene().OnRecalculationFinished();
+        }
+    } else {
+        if (!is_first_time_ready) {
+            ui_manager->GetViewScene().RemoveObject(prev_plot);
+            ui_manager->GetViewScene().AddObject(handler->GetPlot());
+            is_first_time_ready = true;
+        }
+        const auto [width, height] = ui_manager->GetViewScene().GetViewPortSize();
+        handler->SetProgressParams(width, height);
+    }
+    prev_plot = handler->GetPlot();
+}
+
+W2VHandler::Params GetW2VParams(const W2VControlScene::W2VControlState& state) {
+    W2VHandler::Params params{.dimension = static_cast<size_t>(state.dimension),
+                              .neighbours = static_cast<size_t>(state.neighbours),
+                              .epochs = static_cast<size_t>(state.epochs),
+                              .cells = static_cast<size_t>(state.cells),
+                              .min_dist = state.min_dist};
+    for (int i = 0; i < 4; ++i) {
+        params.color[i] = state.color[i];
+    }
+    return params;
+}
+
+void ApplyInput(UIManager* ui_manager, W2VHandler* handler) {
+    auto* controlls = &ui_manager->GetW2VControlScene();
+    const auto [is_new, is_pending] = controlls->IsPendingRecalculation();
+    if (is_new && is_pending) {
+        const auto controlls_state = controlls->GetState();
+        const auto params = GetW2VParams(controlls_state);
+        handler->StartRecalculate(params);
+        controlls->SetRealDimension(0);
+    }
+}
+
+}  // namespace
+
+namespace controllers::w2vhandler {
+
+void Synchronize(UIManager* ui_manager, W2VHandler* handler) {
+    ApplyInput(ui_manager, handler);
+    handler->Update();
+    SynchronizePlot(ui_manager, handler);
+    SynchronizeDetails(ui_manager, handler);
 }
 
 std::unique_ptr<W2VHandler> Initialize(UIManager* ui_manager, const std::string& filename) {
